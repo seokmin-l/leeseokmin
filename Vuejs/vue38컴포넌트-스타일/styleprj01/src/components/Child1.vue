2431d7ff203350200b6a2ac04e06f3c8a229f2df
efadsfaef <35432t3q@aa.co.kr>
2021-07-31 14:02:14 +0900

    중간 커밋

---
+++ Vuejs/vue38컴포넌트-스타일/styleprj01/src/components/Child1.vue
@@ -0,0 +1,20 @@
+<style>
+.main {
+    border: 1px solid black;
+    background-color: green;
+}
+</style>
+<template>
+    <div class="main">
+        {{ msg }}
+    </div>
+</template>
+<script>
+export default {
+    data: function () {
+        return {
+            msg: "Child1"
+        };
+    }
+};
+</script>
---
+++ Vuejs/vue38컴포넌트-스타일/styleprj01/src/components/Child2.vue
@@ -0,0 +1,20 @@
+<style scoped>
+.main {
+    border: 1px solid black;
+    background-color: yellow;
+}
+</style>
+<template>
+    <div class="main">
+        {{ msg }}
+    </div>
+</template>
+<script>
+export default {
+    data: function () {
+        return {
+            msg: "Child2"
+        };
+    }
+};
+</script>
